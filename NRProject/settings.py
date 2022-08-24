2bc599a640baf7e2bfd19feae5b282fd74a7dac6
srincon07 <77310461+srincon07@users.noreply.github.com>
2022-08-24 16:12:45 +0930

    Add configuration for RDS conection

--- NRProject/settings.py
+++ NRProject/settings.py
@@ -71,17 +71,28 @@ WSGI_APPLICATION = 'NRProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+  DATABASES = {
     'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': os.environ['RDS_DB_NAME'],
+      'USER': os.environ['RDS_USERNAME'],
+      'PASSWORD': os.environ['RDS_PASSWORD'],
+      'HOST': os.environ['RDS_HOSTNAME'],
+      'PORT': os.environ['RDS_PORT']
+    },
+  }
+else:
+  DATABASES = {
+      'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'localhost',
         'PORT': '5432'
-    },
-}
-
+      },
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
